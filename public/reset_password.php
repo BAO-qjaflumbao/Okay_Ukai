cfe8036b40b27a2b4084a9fe8fbf3335b1cfdf5f
Jared <qjafiestada@tip.edu.ph>
2025-05-14 02:27:20 +0800

    Updated Reset Password

--- public/reset_password.php
+++ public/reset_password.php
@@ -55,7 +55,7 @@ if (isset($_POST['reset'])) {
     <link rel="stylesheet" href="style/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/reset_password.css">
+    <link rel="stylesheet" href="style/reset.css">
     <title>Reset Password</title>
 </head>
 
