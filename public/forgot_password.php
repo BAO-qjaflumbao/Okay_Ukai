0f6a7d6c009954912c7ed3050c8a344b70f9ff00
Jared <qjafiestada@tip.edu.ph>
2025-05-13 21:30:37 +0800

    .

--- public/forgot_password.php
+++ public/forgot_password.php
@@ -26,7 +26,7 @@ if (isset($_POST['submit'])) {
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="style/forgot_password.css">
+    <link rel="stylesheet" href="style/style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
